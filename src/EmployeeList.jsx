42445a7a037da6f0e4d5d3ef876f863de813dc2b
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-15 14:10:39 -0400

    add new logic

---
+++ src/EmployeeList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import EmployeeListItem from "./EmployeeListItem";
+
+export default function EmployeeList() {
+    const sampleEmployees = [
+        {name: 'James King', title: 'President and CEO'},
+        {name: 'Julie Taylor', title: 'VP of Marketing'},
+        {name: 'Eugene Lee', title: 'CFO'},
+        {name: 'John Williams', title:'VP of Engineering'},
+        {name: 'Ray Moore', title: 'VP of Sales'},
+        {name: 'paul Jones', title: 'QA Manager'}
+    ];
+
+    return (
+        <div className="employee list component">
+            
+        </div>
+    )
+}
