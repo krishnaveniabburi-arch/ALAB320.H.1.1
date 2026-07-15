4f198b147cc9fa2b32e825aa748ad19bf018d8cb
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-15 14:09:57 -0400

    add new logic

---
+++ src/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchBar() {
+    return (
+     <div className="searchBar Comonent">
+        <div className="inner input placeholder">
+
+        </div>
+     </div>
+    );
+}
