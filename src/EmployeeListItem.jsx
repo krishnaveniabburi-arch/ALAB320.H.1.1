c46f0d6b1b4bdbf5e1786dddaf5e5e6c247fc13e
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-15 14:11:19 -0400

    add new logic

---
+++ src/EmployeeListItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function EmployeeListItem({name, title}) {
+    return (
+        <div className="employee list item component">
+            
+        </div>
+    )
+}
